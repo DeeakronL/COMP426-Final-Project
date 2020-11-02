737805121b14f376338ef73e4c4081f3d51db2cd
Luke Wheeler <deeakronplays@gmail.com>
2020-11-01 23:03:10 -0500

    even more stuff

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import View from 'view.js';
-import Model from 'model.js';
+import View from '/view.js';
+import Model from '/model.js';
 
 let model = new Model();
 let view = new View(model);
--- view.js
+++ view.js
@@ -1,11 +1,12 @@
 export default class View {
     constructor(model) {
-        this.div = <div></div>;
+        this.div = $(`<div></div>`);
         let window = $(`<div class=window></div>`)
             .css('position','relative')
             .css('width',  1000 + "px")
-            .css('height', 50 + "px")
+            .css('height', 500 + "px")
             .css('margin','auto')
             .css('background-color','gray');
+        this.div.append(window);
     }
 }
